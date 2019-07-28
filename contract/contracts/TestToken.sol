82ed6a9c7aef506e41959bf0e59b435b508f03de
Rahul Dey <gamefreak85@gmail.com>
2019-07-28 20:44:07 +0800

    removing unneeded comments

--- contract/contracts/TestToken.sol
+++ contract/contracts/TestToken.sol
@@ -7,7 +7,6 @@ contract TestToken is ERC20, ERC20Detailed {
     constructor(string memory name, string memory symbol, uint8 decimals, uint initialSupply)
         ERC20Detailed(name, symbol, decimals)
         public {
-            // _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
             _mint(msg.sender, initialSupply);
         }
 }
